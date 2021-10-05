9b38caa95207470c3dda0ae2f6e0b328e52d5217
cgkaczenski <kzpulaski@gmail.com>
2021-10-04 22:59:09 -0500

    complete lesson

--- block.js
+++ block.js
@@ -1,35 +1,27 @@
 /**
  * Import crypto-js/SHA256 library
  */
-const SHA256 = require('crypto-js/sha256');
+import sha256 from "crypto-js/sha256.js";
 
 /**
- * Class with a constructor for block 
+ * Class with a constructor for block
  */
 class Block {
+  constructor(data) {
+    this.id = 0;
+    this.nonce = 144434;
+    this.body = data;
+    this.hash = "";
+  }
 
-	constructor(data){
-		this.id = 0;
-        this.nonce = 144444;
-      	this.body = data;
-      	this.hash = "";
-    }
-    
-    /**
-     * Step 1. Implement `generateHash()`
-     * method that return the `self` block with the hash.
-     * 
-     * Create a Promise that resolve with `self` after you create 
-     * the hash of the object and assigned to the hash property `self.hash = ...`
-     */
-  	// 
-  	generateHash() {
-      	// Use this to create a temporary reference of the class object
-      	let self = this;
-        //Implement your code here
-        
-    }
+  generateHash() {
+    // Use this to create a temporary reference of the class object
+    let self = this;
+    return new Promise(function (resolve, reject) {
+      self.hash = sha256(JSON.stringify(self));
+      resolve(self);
+    });
+  }
 }
 
-// Exporting the class Block to be reuse in other files
-module.exports.Block = Block;
+export default Block;
